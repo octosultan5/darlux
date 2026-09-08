303b52d15696a982358bbf6b4c2e01d9046b278e
octokarimi5 <octokarimi5@gmail.com>
2026-09-09 00:46:55 +0100

    feat: hardcode meta pixel id arwashop1 for quick activation

--- src/app/components/Pixels.tsx
+++ src/app/components/Pixels.tsx
@@ -3,7 +3,7 @@
 import Script from "next/script";
 
 export default function Pixels() {
-  const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+  const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "841981433961962";
   const TIKTOK_PIXEL_ID = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID;
 
   return (
